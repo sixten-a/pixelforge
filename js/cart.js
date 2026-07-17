1d90972c14704b3bda9deac9683484f4f9ab5bb8
sixten-a <sixten.alasaarela@gmail.com>
2026-07-17 05:56:42 +0000

    site upd

---
+++ js/cart.js
@@ -0,0 +1,12 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " lisättiin ostoskoriin!");
+}
